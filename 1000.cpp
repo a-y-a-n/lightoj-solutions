d36ca057f15b745e0074e153f2a500fb46438b65
A <aro.bd.mail@gmail.com>
2021-01-12 02:01:10 +0000

    simple addition

---
+++ 1000.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin>>t;
+	for(int cs=1;cs<=t;cs++)
+	{
+		int a,b;
+		cin>>a>>b;
+		cout<<"Case "<<cs<<": ";
+		cout<<a+b<<"\n";
+		
+	}
+}
+
