7a0997ee4693d7fb1a19e14d5118ce8413b03b94
A <aro.bd.mail@gmail.com>
2021-01-23 04:21:58 +0000

    warmup

---
+++ 1294.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+   int t;
+   scanf("%d",&t);
+   for(int cs=1;cs<=t;cs++)
+   {
+	long long n,m;
+	scanf("%lld%lld",&n,&m);
+	printf("Case %d: %lld\n",cs,(n*m)/2);
+	
+	}
+}
